8720309d9b09a7ff66ab16379d670243cd28cfb6
Raj Chimmalgi <chimmalgi@mgail.com>
2023-09-08 00:25:14 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+import Tours from "./component/Tours";
+import { Services } from "./component/Services";
+import Footer from "./component/Footer";
+import About from "./component/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/component/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>continue exploring</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
+        </p>
+        <a href="src/component/Hero#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
